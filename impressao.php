d35597e5adb89dd795ab0a722bf3c7726e2e52c5
correar <rodrigo.tornaciole@gmail.com>
2018-06-20 14:40:24 -0300

    remover arquivos refatorados

--- impressao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("conf/db.php");
-	include_once("app/models/Model_impressao.php");
-	include_once("app/controller/Impressao.php");
-	include "app/views/themes/header.php";
-	$impressao = new Impressao();
-	include "app/views/impressao/index.php";
-	include "app/views/themes/footer.php";
-?>		
