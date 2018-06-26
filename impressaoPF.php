5aa5b5d68130fad02a663932778d27fbe07e581d
correar <rodrigo.tornaciole@gmail.com>
2018-06-26 13:37:23 -0300

    Delete impressaoPF.php

--- impressaoPF.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("conf/db.php");
-	include_once("app/models/Model_impressao.php");
-	include_once("app/controller/ImpressaoPF.php");
-	include "app/views/themes/header.php";
-	$impressao = new ImpressaoPF();
-	include "app/views/impressao_pf/index.php";
-	include "app/views/themes/footer.php";
-?>		
