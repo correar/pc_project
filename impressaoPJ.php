113d0ab6b63d63f0fad14ff532b5ac70fecabf22
correar <rodrigo.tornaciole@gmail.com>
2018-06-26 13:37:32 -0300

    Delete impressaoPJ.php

--- impressaoPJ.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("conf/db.php");
-	include_once("app/models/Model_impressao.php");
-	include_once("app/controller/ImpressaoPJ.php");
-	include "app/views/themes/header.php";
-	$impressao = new ImpressaoPJ();
-	include "app/views/impressao_pj/index.php";
-	include "app/views/themes/footer.php";
-?>		
